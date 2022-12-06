e8baa0384060c78e1c82bf13758a4d9726a9cec4
Wulitou-wahaha <77608037+Wulitou-wahaha@users.noreply.github.com>
2022-12-07 00:47:04 +0800

    Update sijishe.py

--- sijishe.py
+++ sijishe.py
@@ -9,19 +9,16 @@ def check_in_sijishe():
     try:
         driver = get_web_driver()
         driver.get("https://sijisheb.com/k_misign-sign.html")
-        driver.find_element_by_xpath("//*[@id='JD_sign']").click()
-        time.sleep(5)
+        driver.find_element_by_xpath("//*[@class='btn J_chkitot']").click()
+        time.sleep(3)
         driver.find_element_by_xpath("//*[@name='username']").send_keys(username)
         driver.find_element_by_xpath("//*[@name='password']").send_keys(password)
-        time.sleep(5)
+        time.sleep(3)
         driver.find_element_by_xpath("//*[@type='submit']").click()
         time.sleep(3)
         driver.get("https://sijisheb.com/k_misign-sign.html")
-        if driver.find_elements_by_xpath("//*[@class='btn J_chkitot']") != []:
         driver.find_element_by_xpath("//*[@class='btn J_chkitot']").click()
         print('司机社签到')
-        else         
-        print('今日已签到')
     except:
         raise
     finally:
