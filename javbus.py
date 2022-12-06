9ac0652093648abb0e8d318095c0976cdb7d3314
Wulitou-wahaha <77608037+Wulitou-wahaha@users.noreply.github.com>
2022-12-06 19:20:32 +0800

    Update javbus.py

--- javbus.py
+++ javbus.py
@@ -1,1 +1,22 @@
+from util import *
 
+username = sys.argv[1] # 登录账号
+password = sys.argv[2] # 登录密码
+img_path = os.getcwd() + "/1.png"
+
+@retry(stop_max_attempt_number=5)
+def check_in_javbus():
+    try:
+        driver = get_web_driver()
+        driver.get("https://www.javbus.com/forum/member.php?mod=logging&action=login")
+        driver.find_element_by_xpath("//*[@name='username']").send_keys(username)
+        driver.find_element_by_xpath("//*[@name='password']").send_keys(password)
+        driver.find_element_by_xpath("//*[@type='submit']").click()
+        print('javbus签到成功')
+    except:
+        raise
+    finally:
+        driver.quit()
+
+if __name__ == '__main__':
+    check_in_javbus()
