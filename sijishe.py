3460202ccb30c045b09305c9c2ff9934bd6eebf1
Wulitou-wahaha <149359246@qq.com>
2022-12-08 00:12:49 +0800

    change sijishe.py

--- sijishe.py
+++ sijishe.py
@@ -9,19 +9,16 @@ def check_in_sijishe():
     try:
         driver = get_web_driver()
         driver.get("https://sijisheb.com/k_misign-sign.html")
-        driver.find_element_by_xpath("//*[@class='btn J_chkitot']").click()
+        driver.find_element_by_xpath("//*[@id="JD_sign"]").click()
         time.sleep(3)
-        driver.find_element_by_xpath("//*[@name='username']").send_keys(username)
-        driver.find_element_by_xpath("//*[@name='password']").send_keys(password)
+        driver.find_element_by_xpath("//input[@name='username']").send_keys(username)
+        driver.find_element_by_xpath("//input[@name='password']").send_keys(password)
         time.sleep(3)
-        driver.find_element_by_xpath("//*[@type='submit']").click()
+        driver.find_element_by_xpath("//button[@type='submit']").click()
         time.sleep(3)
         driver.get("https://sijisheb.com/k_misign-sign.html")
-        if driver.find_element_by_xpath("//*[@class='btn J_chkitot']"):
-        driver.find_element_by_xpath("//*[@class='btn J_chkitot']").click()
+        driver.find_element_by_xpath("//*[@id="JD_sign"]").click()
         print('司机社签到')
-        if driver.find_element_by_xpath("//*[@class='btn btnvisted']"):
-        print('今日已签到')  
     except:
         raise
     finally:
