861f4ae8d34db7bd7f8f9371c1dc6405d479e297
Wulitou-wahaha <149359246@qq.com>
2022-12-07 23:55:31 +0800

    change xpath

--- javbus.py
+++ javbus.py
@@ -11,7 +11,7 @@ def check_in_javbus():
         driver.find_element_by_xpath("//input[@name='username']").send_keys(username)
         driver.find_element_by_xpath("//input[@name='password']").send_keys(password)
         time.sleep(5)
-        valid = Ocr_Captcha(driver, "//*[@id="vseccode_cSlELIzs"]/img", img_path)
+        valid = Ocr_Captcha(driver, "//*[@id="vseccode_cSlELIzs"]", img_path)
         driver.find_element_by_xpath("//input[@name='seccodeverify']").send_keys(valid)
         driver.find_element_by_name("//button[@type='submit']").click()
         time.sleep(3)
