cd70d85e0b7468366923bd1833f9221f48d82065
Wulitou-wahaha <149359246@qq.com>
2022-12-08 15:14:54 +0800

    change method

--- util.py
+++ util.py
@@ -30,9 +30,7 @@ def is_visible(driver, locator, timeout=10):
     except TimeoutException:
         return False
 
-def Ocr_Captcha(driver, locator): # 验证码识别
-    propertery = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, locator)))
-    img_path=propertery.get_attribute(src)
+def Ocr_Captcha(driver, propertery, img_path): # 验证码识别
     driver.save_screenshot(img_path)
     img = Image.open(img_path)
     location = propertery.location
