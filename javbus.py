ea9cee08cd28a4c4995e2cb99ebbf67346accddf
Wulitou-wahaha <149359246@qq.com>
2022-12-08 16:47:03 +0800

    fix error

--- javbus.py
+++ javbus.py
@@ -16,10 +16,10 @@ def check_in_javbus():
         driver.find_element_by_xpath("//button[@type='submit']").click()
         time.sleep(3)
         driver.get("https://www.javbus.com/forum/home.php?mod=spacecp&ac=credit")
-        try
+        try:
         driver.find_element_by_xpath("//[@id="ct"]/div[1]/div/ul[2]/li[1]/text()")
         print('登录成功')
-        except
+        except:
         print('登陆失败')
     except:
         raise
